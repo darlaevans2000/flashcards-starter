ad218583e32d02094d2d76dd99febb9bf557eb52
darlaevans2000 <darlaevans2000@gmail.com>
2021-07-07 15:36:27 -0600

    Add giveFeedback method

--- src/Turn.js
+++ src/Turn.js
@@ -3,4 +3,22 @@ class Turn {
         this.guess = guess;
         this.card = card;
     }
+
+    returnGuess() {
+        return this.guess;
+    }
+
+    returnCard() {
+        return this.card;
+    }
+
+    evaluateGuess() {
+        return this.guess === this.card.correctAnswer ? true : false
+        // using ternary operator ? as an if statement. If this.guess is strictly equal to 
+        // the correct answer it will return our first option, true, else it returns false.
+    }
+
+    giveFeedback() {
+        return this.evaluateGuess() === true ? "correct!" : "incorrect!"
+    }
 }
